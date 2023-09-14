1fcee3c8522cc68585ae1432415b86089064db16
vesear <veselkoartyom@gmail.com>
2023-09-14 17:11:40 +0200

    Added mailosaur client, completed registration process

--- test/config.js
+++ test/config.js
@@ -1,1 +1,7 @@
 export const APP_URL = process.env.APP_URL;
+
+export const MAILOSAUR = {
+    API_KEY: process.env.MAILOSAUR_API_KEY,
+    SERVER_ID: process.env.MAILOSAUR_SERVER_ID,
+    DOMAIN: process.env.MAILOSAUR_DOMAIN,
+};
--- test/specs/registration.e2e.js
+++ test/specs/registration.e2e.js
@@ -1,10 +1,40 @@
 import { expect } from 'chai';
+import { faker } from '@faker-js/faker';
+import MailosaurClient from 'mailosaur';
 
 import { RegistrationPage } from '../pageobjects/signUp/registrationPage.js';
+import { OtpPage } from '../pageobjects/signup/otpPage.js';
+import { CreatePasswordPage } from '../pageobjects/signup/createPasswordPage.js';
+import { InvoicesPage } from '../pageobjects/signup/invoicesPage.js';
+import { extractNumberFromString } from '../utils/extractNumberFromString.js';
+import { MAILOSAUR, APP_URL } from '../config.js';
+
+const { API_KEY, SERVER_ID, DOMAIN } = MAILOSAUR;
+const mailosaur = new MailosaurClient(API_KEY);
+
+const username = faker.internet.displayName();
+const password = faker.internet.password();
+const email = `${username}@${SERVER_ID}.${DOMAIN}`;
 
 describe('Registration', () => {
     it('should register a new user', async () => {
         await browser.url('sign-up');
         expect(await RegistrationPage.isOpened()).to.be.true;
+
+        await RegistrationPage.enterEmail(email);
+        expect(await OtpPage.isOpened()).to.be.true;
+
+        const { subject } = await mailosaur.messages.get(SERVER_ID, { sentTo: email });
+        const otpCode = extractNumberFromString(subject);
+        await browser.keys([...otpCode]);
+        await OtpPage.clickContinue();
+        expect(await CreatePasswordPage.isOpened()).to.be.true;
+
+        await CreatePasswordPage.enterPassword(password, password);
+        await browser.pause(3000);
+
+        expect(await InvoicesPage.isOpened()).to.be.true;
+        const currentPageUrl = await browser.getUrl();
+        expect(currentPageUrl).to.eql(`${APP_URL}invoices`);
     });
 });
